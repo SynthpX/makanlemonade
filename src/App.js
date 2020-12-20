14e9930be758495f539c9abf8482da74966f78ff
SynthpX <adliakbarmoh@gmail.com>
2020-12-21 03:14:11 +0700

    halaman login complete

--- src/App.js
+++ src/App.js
@@ -2,20 +2,18 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './halaman/homepage/homepage.component';
-
-
-const pageTopi = () => (
-  <div>
-    <h1>Halaman Topi</h1>
-  </div>
-);
+import ShopPage from './halaman/shop/shop.component.jsx';
+import Header from './komponen/header/header.component.jsx';
+import SignInAndSignUpPage from './halaman/signin-and-signup/signin-and-signup.component';
 
 function App() {
   return(
     <div>
+      <Header/>
     <Switch>
       <Route exact path='/' component={HomePage}/>
-      <Route path='/topi' component={pageTopi}/>
+      <Route path='/shop' component={ShopPage}/>
+      <Route path ='/signin' component={SignInAndSignUpPage}/>
     </Switch>
   </div>
   );
---
+++ src/halaman/signin-and-signup/signin-and-signup.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './signin-and-signup.style.scss';
+import SignIn from '../../komponen/sign-in/sign-in.component';
+
+const SignInAndSignUpPage = () => (
+    <div className='signin-and-signup'>
+        <SignIn/>
+    </div>
+);
+
+export default SignInAndSignUpPage;
